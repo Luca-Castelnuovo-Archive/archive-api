98911be54681a24e2f84bc417d646b0a163a3414
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-17 17:27:17 +0100

    update

--- includes/authentication.php
+++ includes/authentication.php
@@ -1,6 +1,13 @@
 <?php
 
-//authenticate access token
-// header
-// post
-// get
+function authenticate_access($access_GET, $access_POST, $access_HEADER)
+{
+    if (empty($access_GET) && empty($access_POST) && empty($access_HEADER)) {
+        response(false, 'access_token_empty');
+    }
+
+    $token_valid = true;
+    if (!$token_valid) {
+        response(false, 'bad_credentials');
+    }
+}
