897f958f23b9bfc27abd2bfbe59abca0881ad001
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 14:35:58 +0100

    updtae

--- users/index.php
+++ users/index.php
@@ -2,10 +2,11 @@
 
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/init.php';
 
-$access_token = authenticate_access($_REQUEST['access_token'], $_SERVER['Authorization']);
+// $access_token = authenticate_access($_REQUEST['access_token'], $_SERVER['Authorization']);
 
-$access = sql_select('users', '*', "id='{$access_token['user_id']}'", true);
+// $user = sql_select('users', '*', "id='{$access_token['user_id']}'", true);
+$user = sql_select('users', '*', "id='12'", true);
 
-print_r($access);
 
-response(true, 'access_granted', ['user' => $access]);
+print_r($user);
+response(true, 'access_granted', ['user' => $user]);
