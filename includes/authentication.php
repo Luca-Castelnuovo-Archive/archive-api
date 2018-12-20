0800bf0676abb2ca5f4273e7d2717d238309f7e9
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 14:58:04 +0100

    update

--- includes/authentication.php
+++ includes/authentication.php
@@ -7,26 +7,15 @@ function authenticate_access($access_REQUEST, $access_HEADER, $required_scope = 
     $access_token = check_data($_REQUEST['access_token'], true, 'access_token', true);
 
     // Query token
-    $access = sql_select('access_tokens', 'client_id,user_id,expires,scope', "access_token='{$access_token}'", true);
-    $scopes = json_decode($access_token['scope']);
+    $access_data = sql_select('access_tokens', 'client_id,user_id,expires,scope', "access_token='{$access_token}'", true);
+    $scope = json_decode($access_data['scope']);
 
     // Check if expires
     if ($access['expires'] <= time()) {
         response(false, 'bad_access_token');
     }
 
-    // Validate token
-    // $token_valid = false;
-    // foreach ($scopes as $scope) {
-    //     if ($scope == $required_scope) {
-    //         $token_valid = true;
-    //         break;
-    //     }
-    // }
-    //
-    // if (!$token_valid) {
-    //     response(false, 'bad_access_token');
-    // }
+    // Check token scope
 
-    return ['client_id' => $access_token['client_id'], 'user_id' => $access_token['user_id'], 'expires' => $access_token['expires'], 'scope' => $scope];
+    return ['client_id' => $access_data['client_id'], 'user_id' => $access_data['user_id'], 'expires' => $access_data['expires'], 'scope' => $scope];
 }
