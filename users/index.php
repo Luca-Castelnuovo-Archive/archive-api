4d8fc49d1510018bd475b50df813822e672222b8
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 15:10:17 +0100

    update

--- users/index.php
+++ users/index.php
@@ -4,7 +4,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/init.php';
 
 $access_token = authenticate_access($_REQUEST['access_token'], $_SERVER['Authorization']);
 
-$user = sql_select('users', '*', "id='{$access_token['user_id']}'", true);
+$user = sql_select('users', 'id,username,email,first_name,last_name,picture_url,created,applications', "id='{$access_token['user_id']}'", true);
 
 $user['applications'] = json_decode($user['applications'], true);
 
