82e2e9a1649c968670fedbc01a9814444d25e9df
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 12:59:28 +0100

    update

--- users/index.php
+++ users/index.php
@@ -6,4 +6,6 @@ $access_token = authenticate_access($_REQUEST['access_token'], $_SERVER['Authori
 
 $access = sql_select('users', '*', "id='{$access_token['user_id']}'", true);
 
-response(true, 'access_granted', $access);
+print_r($access);
+
+response(true, 'access_granted', ['user' => $access]);
