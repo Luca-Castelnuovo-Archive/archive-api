2746a3ab5809cead4293bf43d1fbf806294de478
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 15:14:45 +0100

    update

--- includes/authentication.php
+++ includes/authentication.php
@@ -2,11 +2,11 @@
 
 function authenticate_access($access_REQUEST, $access_HEADER, $required_scope = null)
 {
-    if (!isset($access_REQUEST) && empty($access_REQUEST) && !isset($access_HEADER) && empty($access_HEADER)) {
-        response(false, 'bad_access_token');
-    } elseif (!isset($access_REQUEST) && empty($access_REQUEST)) {
-        $token = $access_HEADER;
-    } elseif (!isset($access_HEADER) && empty($access_HEADER)) {
+    if (empty($access_REQUEST) && empty($access_HEADER)) {
+        response(false, 'missing_access_token');
+    } elseif (!empty($access_REQUEST)) {
+        $token = $access_REQUEST;
+    } elseif (!empty($access_HEADER)) {
         $token = $access_HEADER;
     }
 
@@ -15,7 +15,6 @@ function authenticate_access($access_REQUEST, $access_HEADER, $required_scope = 
     $access_data = sql_select('access_tokens', 'client_id,user_id,expires,scope', "access_token='{$token}'", true);
     $scope = json_decode($access_data['scope']);
 
-    // Check if expires
     if ($access_data['expires'] <= time()) {
         response(false, 'bad_access_token');
     }
