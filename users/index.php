98461681b0f6e24a66eb2c9235c872e3f1625a5b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-20 14:37:15 +0100

    update

--- users/index.php
+++ users/index.php
@@ -7,6 +7,4 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/init.php';
 // $user = sql_select('users', '*', "id='{$access_token['user_id']}'", true);
 $user = sql_select('users', '*', "id='12'", true);
 
-
-print_r($user);
 response(true, 'access_granted', ['user' => $user]);
