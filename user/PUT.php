0cd3e8a919f734a31722f59b9bea08ef6f508b04
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-21 20:35:32 +0100

    update

--- user/PUT.php
+++ user/PUT.php
@@ -11,18 +11,42 @@ function PUT_user($scope, $user_id, $picture_url, $username, $first_name, $last_
     $last_name = check_data($last_name, false, '', true);
     $email = check_data($email, false, '', true);
 
-    $user = sql_select('users', 'id,username,email,first_name,last_name,picture_url,created,applications', "id='{$user_id}'", true);
+    $user = sql_select('users', 'username,email', "id='{$user_id}'", true);
 
-    // Check if username is taken
-    if ($username != $user['username']) {
-        $existing_username = sql_select('users', 'id', "username='{$username}'", false);
-        if ($existing_username->num_rows > 0) {
-            response(false, 'username_already_taken');
-        }
+    if (!empty($picture_url)) {
+        sql_update('users', [
+            'picture_url' => $picture_url,
+        ], "id='{$user_id}'");
     }
 
-    if (scope_allowed($scope, 'email', false)) {
-        // Check if email is taken
+    if (!empty($username)) {
+        if ($username != $user['username']) {
+            $existing_username = sql_select('users', 'id', "username='{$username}'", false);
+            if ($existing_username->num_rows > 0) {
+                response(false, 'username_already_taken');
+            }
+        }
+
+        sql_update('users', [
+            'username' => $username,
+        ], "id='{$user_id}'");
+    }
+
+    if (!empty($first_name)) {
+        sql_update('users', [
+            'first_name' => $first_name,
+        ], "id='{$user_id}'");
+    }
+
+    if (!empty($last_name)) {
+        sql_update('users', [
+            'last_name' => $last_name,
+        ], "id='{$user_id}'");
+    }
+
+    if (!empty($email)) {
+        scope_allowed($scope, 'email', true);
+
         if ($email != $user['email']) {
             $existing_email = sql_select('users', 'id', "email='{$email}'", false);
             if ($existing_email->num_rows > 0) {
@@ -34,13 +58,6 @@ function PUT_user($scope, $user_id, $picture_url, $username, $first_name, $last_
             'email' => $email,
         ], "id='{$user_id}'");
     }
-
-    sql_update('users', [
-        'username' => $username,
-        'first_name' => $first_name,
-        'last_name' => $last_name,
-        'picture_url' => $picture_url,
-    ], "id='{$user_id}'");
 
     return GET_user($user_id);
 }
