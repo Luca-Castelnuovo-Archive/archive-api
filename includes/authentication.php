87bcd2bee4cce2a68ef7591bf57d55bce5f2cb2c
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-21 12:01:08 +0100

    update

--- includes/authentication.php
+++ includes/authentication.php
@@ -27,7 +27,6 @@ function validate_access($access_REQUEST, $access_HEADER, $required_scope = null
 }
 
 function scope_allowed($scopes, $required_scope, $hard_fail = false) {
-    // Example $required_scope='basic:read'
     if (strpos($required_scope, ':') !== false) {
         if (!in_array($required_scope, $scopes)) {
             $required_scope_without_read = substr($required_scope, 0, strpos($required_scope, ":"));
@@ -39,14 +38,13 @@ function scope_allowed($scopes, $required_scope, $hard_fail = false) {
                 }
             }
         }
-    }
-
-    // Exaple $required_scope='basic'
-    if (!in_array($required_scope_read, $scopes)) {
-        if ($hard_fail) {
-            response(false, 'request_out_of_scope');
-        } else {
-            return false;
+    } else {
+        if (!in_array($required_scope, $scopes)) {
+            if ($hard_fail) {
+                response(false, 'request_out_of_scope');
+            } else {
+                return false;
+            }
         }
     }
 
