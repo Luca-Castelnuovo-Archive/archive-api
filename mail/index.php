d7e1ec84217b40d77824f2e31cad9ac3443fe8aa
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-02 16:00:04 +0100

    update

--- mail/index.php
+++ mail/index.php
@@ -21,7 +21,7 @@ require 'vendor/autoload.php';
 
 //Server configuration
 $mail = new PHPMailer(true);
-$mail->SMTPDebug = 2;
+// $mail->SMTPDebug = 2;
 $mail->isSMTP();
 $mail->CharSet = 'UTF-8';
 $mail->Host = $GLOBALS['config']->services->mail->Host;
