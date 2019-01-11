0a8108b739f783a7fd48f4d7ee08410f3dbba4b7
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-12 00:54:47 +0100

    test

--- includes/init.php
+++ includes/init.php
@@ -6,3 +6,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/authentication.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/output.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/security.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/sql.php';
+
+// External
+require '/var/www/logs.lucacastelnuovo.nl/public_html/logs.php';
+log_action('1', 'service.test', $_SERVER["REMOTE_ADDR"], 'USER_ID', 'CLIENT_ID');
