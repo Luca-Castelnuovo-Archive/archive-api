a623b28f69a5bb5ab1b73f63bab5d2e17d712b52
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-12 01:20:58 +0100

    dp

--- includes/init.php
+++ includes/init.php
@@ -9,11 +9,4 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/sql.php';
 
 // External
 require '/var/www/logs.lucacastelnuovo.nl/public_html/logs.php';
-
-if (function_exists('log_action')) {
-    response(true);
-} else {
-    response(false);
-}
-
 log_action('1', 'service.test', $_SERVER["REMOTE_ADDR"], 'USER_ID', 'CLIENT_ID');
