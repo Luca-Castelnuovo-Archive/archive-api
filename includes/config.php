e0774a6b0c475c1996be82a798ff2790e33fb093
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-08-24 21:53:04 +0200

    update

---
+++ includes/config.php
@@ -0,0 +1,42 @@
+<?php
+
+$configKey = getenv('CONFIG_KEY');
+$configClient = new \ConfigCat\ConfigCatClient($configKey);
+
+if (!$configClient->getValue("appActive", false)) {
+    http_response_code(503);
+    exit('App is temporarily disabled.');
+}
+
+return (object) array(
+    'database' => (object) array(
+        'host' => $configClient->getValue("dbHost", "localhost"),
+        'user' => $configClient->getValue("dbUser", ""),
+        'password' => $configClient->getValue("dbPassword", ""),
+        'database' => $configClient->getValue("dbDatabase", ""),
+    ),
+
+    'auth' => (object) array(
+        'client_id' => $configClient->getValue("clientID", ""),
+        'client_secret' => $configClient->getValue("clientSecret", ""),
+    ),
+
+    'services' => (object) array(
+        'mail' => (object) array(
+            'Host' => $client->getValue("mailHost", ""),
+            'SMTPAuth' => $client->getValue("mailSMTPAuth", true),
+            'Username' => $client->getValue("mailUsername", ""),
+            'Password' => $client->getValue("mailPassword", ""),
+            'SMTPSecure' => $client->getValue("mailSMTPSecure", "ssl"),
+            'Port' => $client->getValue("mailPort", 465),
+            'From' => $client->getValue("mailFrom", ""),
+        ),
+        'recaptcha' => (object) array(
+            'secret_key' => $client->getValue("recaptchaSecretKey", ""),
+        ),
+        'url' => (object) array(
+            'signature' => $client->getValue("urlSignature", ""),
+        ),
+    ),
+);
+
