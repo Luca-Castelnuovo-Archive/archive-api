93617fedfba21a8dacdfbbbc74e6a5121502a904
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-08-26 21:27:42 +0200

    update

--- includes/init.php
+++ includes/init.php
@@ -10,4 +10,4 @@ require $_SERVER['DOCUMENT_ROOT'] . '/includes/security.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/sql.php';
 
 // External
-require '/var/www/logs.lucacastelnuovo.nl/public_html/logs.php';
+require '/var/www/logs.lucacastelnuovo.nl/logs.php';
